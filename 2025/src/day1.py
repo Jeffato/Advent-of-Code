b5c45d5cdbc57dd9d6a99e766855ff16ea466799
Jeffato <jlb671@scarletmail.rutgers.edu>
2025-12-13 21:53:51 -0500

    Day 1 boilerplate

---
+++ 2025/src/day1.py
@@ -0,0 +1,14 @@
+'''
+Input: txt file
+ -Each line has L/R and a # representing a rotation on a dial
+    -L -> move down the dial
+    -R -> move up the dial
+    -# -> number, can be big
+
+Dial Features
+ - Starts from 0 and goes to 99. Loops back to 0 from 99
+
+Part 1 Output: # of times dial lands on 0 after a completed rotation step
+
+Part 2 Output: # of times dial passes 0
+'''
